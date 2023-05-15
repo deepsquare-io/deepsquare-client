e45299f198af6232344d23f94f377a8001dd6b5a
Nguyen Marc <nguyen_marc@live.fr>
2023-05-15 19:39:32 +0200

    fix: fix test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,7 +5,7 @@ import DeepSquareClient from '.';
 describe('DeepSquareClient', () => {
   it('should initialize', async () => {
     const wallet = ethers.Wallet.createRandom();
-    DeepSquareClient.build(
+    await DeepSquareClient.build(
       wallet._signingKey().privateKey,
       '0x77ae38244e0be7cFfB84da4e5dff077C6449C922'
     );
