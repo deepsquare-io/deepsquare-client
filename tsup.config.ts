5285cd2198c1b5ce510b8c0371e0b156a81653b0
Nguyen Marc <nguyen_marc@live.fr>
2023-11-21 22:22:37 +0100

    fix(tsup): do not treeshake nor minify

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
     "src/grpc/browser/index.ts",
   ],
   dts: true, // Generate .d.ts files
-  minify: true, // Minify output
+  minify: false, // Minify output
   sourcemap: true, // Generate sourcemaps
-  treeshake: true, // Remove unused code
   splitting: true, // Split output into chunks
   clean: true, // Clean output directory before building
   platform: "browser",
