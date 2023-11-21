fa48790ea9049bc58444b5787a7733ce157ddc62
Nguyen Marc <nguyen_marc@live.fr>
2023-11-21 22:01:50 +0100

    feat(npm): export package wagmi style

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,8 +6,13 @@ export default defineConfig({
     "src/grpc/node/index.ts",
     "src/grpc/browser/index.ts",
   ],
-  splitting: false,
-  sourcemap: true,
-  minify: true,
-  clean: true,
+  dts: true, // Generate .d.ts files
+  minify: true, // Minify output
+  sourcemap: true, // Generate sourcemaps
+  treeshake: true, // Remove unused code
+  splitting: true, // Split output into chunks
+  clean: true, // Clean output directory before building
+  platform: "browser",
+  outDir: "dist", // Output directory
+  format: ["esm"], // Output format(s)
 });
